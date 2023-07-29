e28456dc33b35ef68708fc76c5e3c14f6901d1c4
First-light <15779212418@qq.com>
2023-07-29 16:46:41 +0800

    完善了指标函数

--- USER/task_list.c
+++ USER/task_list.c
@@ -6,6 +6,9 @@
 RunningStruct MainControlRun = 
 {AT_BRAKE,LINE_CC,TEST_ON,MANUAL_OFF};
 
+#define CIRCLE_ON 1
+#define CIRCLE_OFF 0
+
 MoveStruct AutoMoveList[] ={
 	{90,0,0},
 	{100,0,0},
@@ -15,7 +18,7 @@ MoveStruct AutoMoveList[] ={
 };
 
 AttitudeTypedef AutoTaskList[] ={
-	AT_MANUAL,
+	AT_MANUAL_TEST,
 	AT_AUTO_TESTSTART,
 	AT_AUTO_FOLLOWLINE_SHORT,
 	AT_AUTO_FINDPOINT,
@@ -35,6 +38,22 @@ AttitudeTypedef AutoTaskList[] ={
 };
 
 
+uint8_t TaskTarget(AttitudeTypedef task)
+{
+	uint8_t circle = CIRCLE_ON;
+	switch (task)
+	{
+		case AT_MANUAL:
+			if(MainControlRun.ManualMode == MANUAL_OFF)circle = CIRCLE_OFF;
+			break;
+		case AT_MANUAL_TEST:
+			if(MainControlRun.ManualMode == MANUAL_OFF)circle = CIRCLE_OFF;
+			break;
+		default:circle = CIRCLE_OFF;
+	}		
+	return circle;
+}
+
 
 void TaskTodoList (void *p_arg)
 {
@@ -52,13 +71,13 @@ void TaskTodoList (void *p_arg)
 			case AT_MANUAL:
 				MainControlRun.TestMode = TEST_OFF;
 				MainControlRun.ManualMode = MANUAL_ON;
-				while(MainControlRun.ManualMode == MANUAL_ON)
-				OSTimeDlyHMSM(0, 0, 0, 50, OS_OPT_TIME_HMSM_STRICT, &err);
+				while(TaskTarget(list[TodoListNum]))
+				OSTimeDlyHMSM(0, 0, 0, 50, OS_OPT_TIME_HMSM_STRICT, &err);				
 				break;
 			case AT_MANUAL_TEST:
 				MainControlRun.TestMode = TEST_ON;
 				MainControlRun.ManualMode = MANUAL_ON;
-				while(MainControlRun.ManualMode == MANUAL_ON)
+				while(TaskTarget(list[TodoListNum]))
 				OSTimeDlyHMSM(0, 0, 0, 50, OS_OPT_TIME_HMSM_STRICT, &err);
 				break;
 			default :
